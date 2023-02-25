cae2ac882ec7f6ef310a85684e2fe98264f30534
Jose Renteria <120423283+joserenter1a@users.noreply.github.com>
2023-02-24 17:04:54 -0800

    Commit

--- MT_Review/mt_review_scratch2.py
+++ MT_Review/mt_review_scratch2.py
@@ -5,31 +5,46 @@ Author: Jose Renteria
 
 """
 
+
 def func(i: int):
     for el in range(i):
         el += i
     return el
 
+
 print(func(6))
+
+print("-" * 15)
 
 s1 = 'literal'
 s2 = ['l', 'i', 't', 'e', 'r', 'a', 'l']
-(s2.sort())
-s1.sort()
-print(s2)
-print(1 if 'liter' in s1 and s2 else 0) 
+
+print(s2.sort())  # sort alphabetically
+
+print(1 if 'litrla' in s1 and s2 else 0)
+
+print("-" * 15)
 
 t1 = 'ba'
 t2 = ['a', 'b']
+print((t1.index('b')), (t2.index('b')))
 print(min(t1) == min(t2))
+
+print("-" * 15)
 
 v1 = ["kg", "to", "lbs"]
 print("_".join(v1))
 
+print("-" * 15)
 
-def char_exist(vals: str, c: char) -> bool:
-    return c in vals
+
+def char_exist(vals: str, c: str) -> bool:
+    return c in vals # is char in str
+
+
 print(char_exist("gumbo", 'b'))
+
+print("-" * 15)
 
 
 def remove_duplicates(string):
@@ -46,5 +61,16 @@ def remove_duplicates(string):
     # Return the accumulated string with duplicates removed
     return unique
 
+
 with open("sample.txt", 'r') as sample:
-    print(sample.readlines())
+    print(sample.read(2))  # first two characters only
+
+lst = [1,2,3]
+lst[0] = 2
+print(lst)
+
+
+s = "1234"
+print(s, type(s))
+s = (s)
+print(type(s))
