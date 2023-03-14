4c87c4d08c300c9547683e4258dedff99a08fad5
Jose Renteria <120423283+joserenter1a@users.noreply.github.com>
2023-03-14 01:17:24 -0700

    bin3.py

--- binary_search/bin3.py
+++ binary_search/bin3.py
@@ -1,5 +1,5 @@
 """
-Binary Search Practice
+Binary Search Practice and visualization
 CS210-W23
 Author: Jose Renteria
 """
@@ -8,6 +8,7 @@ import random
 import matplotlib.pyplot as plt
 
 
+# simple linear search, you have all seen this style
 def linear_search(arr: list, target: int) -> int:
     for i in range(len(arr)):
         if arr[i] == target:
@@ -16,22 +17,25 @@ def linear_search(arr: list, target: int) -> int:
 
 
 def binary_search(arr: list, target: int, low: int, high: int) -> int:
-    if low > high: # base case, this cannot happen
-        return -1 # exit code
-    else:
-        mid = (low + high) // 2 # mid formula
-        if arr[mid] == target: # if found
-            return mid # return it
-        elif arr[mid] < target: # if the target value is greater
-            return binary_search(arr, target, mid + 1, high) # search the right sublist, shift the middle up by 1
-        else: # if not greater, must be lower
-            return binary_search(arr, target, low, mid - 1) # search the left sublist, shift the middle down by 1
+    pass  # delete when you start
+
+    # TODO
+    # base case, this cannot happen
+    # exit code
+    # mid formula
+    # if found
+    # return it
+    # else if the target value is greater
+    # recursively search the right sublist, shift the middle right by 1
+    # if not greater, must be lower
+    # recursivley search the left sublist, shift the middle left by 1
+
 
 # a visualizer utility to show the time it takes to run iterations of linear vs binary search
 def visualize(arr: list, target: int):
     # Measure the time it takes to run N iterations of linear search
     linear_times = []
-    for i in range(50):
+    for i in range(500):
         start_time = time.time()
         linear_search(arr, target)
         end_time = (time.time())
@@ -39,7 +43,7 @@ def visualize(arr: list, target: int):
 
     # Measure the time it takes to run N iterations of binary search
     binary_times = []
-    for i in range(50):
+    for i in range(500):
         start_time = time.time()
         binary_search(arr, target, 0, len(arr) - 1)
         end_time = time.time()
@@ -51,9 +55,10 @@ def visualize(arr: list, target: int):
     # Create lists containing the average time for each search algorithm
     avg_linear_times = [avg_linear_time] * len(linear_times)
     avg_binary_times = [avg_binary_time] * len(binary_times)
-    
-    print(avg_binary_time)
-    print(avg_binary_time)
+
+    print(f"{round(avg_binary_time * 1000, 5)} seconds")
+    print(f"{round(avg_linear_time * 1000, 5)} seconds")
+    print(f"That's {round(avg_linear_time / avg_binary_time)}x slower using linear search")
 
     # Plot the results
     plt.plot(avg_linear_times, label=' AVG Linear Search')
@@ -67,7 +72,6 @@ def visualize(arr: list, target: int):
 
 
 if __name__ == '__main__':
-
     # Create a sorted list of 10,000 integers
     arr = list(range(10000))
     # Generate a random target value
